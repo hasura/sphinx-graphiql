591186cbe7e053195a0c8a64fdccaee691763e9f
rikinsk <rikin.kachhia@gmail.com>
2018-07-27 16:32:27 +0530

    update release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sphinx_graphiql",
-    version="0.0.2",
+    version="0.0.3",
     author="Rikin K",
     author_email="rikin@hasura.io",
     description="Sphinx extension for GraphiQL",
